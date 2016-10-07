c820ca134a8ded67d457e80641b1b1ba3cac000e
Abdul <arehmandev@outllook.com>
2016-10-07 19:56:42 +0100

    added the mysql external fix for samuel

--- kong.sh
+++ kong.sh
@@ -6,3 +6,5 @@ sleep 22 # modify depending how slow your laptop is
 docker-compose up -d
 echo "Done, run docker-compose up -d if kong didn't load"
 echo "Run docker-compose kill to destroy containers"
+echo "To connect to mysql (password = test)":
+echo " mysql -u root -h $(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CID} mysqldatabase) -p"
